a168317739bfc5bd8c9f80dc4994295f4293a73d
Magnus Freisens <freisensmagnus@test.com>
2022-10-18 22:29:07 +0200

    "create new User -PostgresManagerImpl, -List, -Manager"

---
+++ src/main/java/com/dhbw/RestaurantsReservation/model/user/UserList.java
@@ -0,0 +1,38 @@
+package com.dhbw.RestaurantsReservation.model.user;
+
+import com.dhbw.RestaurantsReservation.dataManager.UserManager;
+import com.dhbw.RestaurantsReservation.dataManagerImpl.PostgresUserManagerImpl;
+import com.dhbw.RestaurantsReservation.model.user.User;
+
+import java.util.Collection;
+
+public class UserList {
+
+    private User user;
+    private Collection<User> users;
+
+    public UserList() { }
+
+    public UserList(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Collection<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers() {
+        UserManager userManager = PostgresUserManagerImpl.getPostgresUserManagerImpl();
+        users = userManager.getAllUsers(new User("Max", "Muster", "test",
+                "Muster@mail.com", 0));
+        this.users = users;
+    }
+}
