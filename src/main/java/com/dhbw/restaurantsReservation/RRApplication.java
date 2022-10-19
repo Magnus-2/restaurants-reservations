28e5ea8fb5f33fbdecb6d8c8b58def68284560d9
Magnus Freisens <freisensmagnus@test.com>
2022-10-19 23:48:26 +0200

    "new start"

--- src/main/java/com/dhbw/restaurantsReservation/RRApplication.java
+++ src/main/java/com/dhbw/restaurantsReservation/RRApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RRApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(RRApplication.class, args);
 	}
