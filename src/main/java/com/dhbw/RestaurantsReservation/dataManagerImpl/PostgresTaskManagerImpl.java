aec9891fd610e2babc3bd88fa4c38aafeb0f02a9
Magnus Freisens <freisensmagnus@test.com>
2022-10-22 21:42:42 +0200

    "change the database"

--- src/main/java/com/dhbw/RestaurantsReservation/dataManagerImpl/PostgresTaskManagerImpl.java
+++ src/main/java/com/dhbw/RestaurantsReservation/dataManagerImpl/PostgresTaskManagerImpl.java
@@ -15,9 +15,9 @@ import java.util.List;
 
 public class PostgresTaskManagerImpl implements TaskManager {
 
-    String databaseURL = "jdbc:postgresql://ec2-34-252-216-149.eu-west-1.compute.amazonaws.com:5432/d9ggrcte17fo7h";
-    String username = "pbyofpxpstoaup";
-    String password = "349a5066ac0eef81cd2f2771a0b722448f2b9222f4832d66a8e78be1627a006b";
+    String databaseURL = "jdbc:postgresql://ec2-52-48-159-67.eu-west-1.compute.amazonaws.com";
+    String username = "supsoxsbvsfzua";
+    String password = "feb1b7ddc20951d1c16581ef1fafb74452611de546824583545b412e11a6bfec";
     BasicDataSource basicDataSource;
 
     static PostgresTaskManagerImpl postgresTaskManager = null;
