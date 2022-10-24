9f5c317272031ac633870b6cc09f7714969d6640
Magnus <toasttv007@gmail.com>
2022-10-24 11:24:55 +0200

    "fixing restaurants"

--- src/main/java/com/dhbw/RestaurantsReservation/model/restaurant/Restaurant.java
+++ src/main/java/com/dhbw/RestaurantsReservation/model/restaurant/Restaurant.java
@@ -1,41 +1,33 @@
 package com.dhbw.RestaurantsReservation.model.restaurant;
 
 
-import lombok.Getter;
-import lombok.Setter;
 
-import javax.persistence.*;
 
-@Table(name = "restauranttable")
+
 public class Restaurant {
 
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
 
-    @Column(name = "rName")
-    @Getter @Setter
+
     private String rName = "";
-    @Column(name = "rSeats")
-    @Getter @Setter
+
+
     private int rSeats = 0;
-    @Column(name = "rZipcode")
-    @Getter @Setter
+
     private int rZipcode = 0;
-    @Column(name = "rAddress")
-    @Getter @Setter
+
+
     private String rAddress = "";
-    @Column(name = "rCategory")
-    @Getter @Setter
+
+
     private String rCategory ="";
-    @Column(name = "rPhone")
-    @Getter @Setter
+
+
     private int rPhone = 0;
-    @Column(name = "rEmail")
-    @Getter @Setter
+
+
     private String rEmail = "";
-    @Column(name = "rPassword")
-    @Getter @Setter
+
     private String rPassword = "";
 
     public Restaurant(){
